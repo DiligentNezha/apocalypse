16772d4e98fe2ae4b4e430c94e789079335ed361
jingkaihui <499508968@qq.com>
2019-06-02 22:57:01 +0800

    sentinel配置修改

--- apocalypse-gateway/src/main/java/com/apocalypse/gateway/config/GatewayConfig.java
+++ apocalypse-gateway/src/main/java/com/apocalypse/gateway/config/GatewayConfig.java
@@ -6,11 +6,9 @@ import com.alibaba.csp.sentinel.adapter.gateway.common.api.ApiPathPredicateItem;
 import com.alibaba.csp.sentinel.adapter.gateway.common.api.ApiPredicateItem;
 import com.alibaba.csp.sentinel.adapter.gateway.common.api.GatewayApiDefinitionManager;
 import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayFlowRule;
-import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayParamFlowItem;
 import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayRuleManager;
 import com.alibaba.csp.sentinel.adapter.gateway.sc.SentinelGatewayFilter;
 import com.alibaba.csp.sentinel.adapter.gateway.sc.exception.SentinelGatewayBlockExceptionHandler;
-import com.alibaba.csp.sentinel.slots.block.RuleConstant;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Bean;
@@ -53,69 +51,14 @@ public class GatewayConfig {
 
     @PostConstruct
     public void doInit() {
-        initCustomizedApis();
         initGatewayRules();
-    }
-
-    private void initCustomizedApis() {
-        Set<ApiDefinition> definitions = new HashSet<>();
-        ApiDefinition api1 = new ApiDefinition("some_customized_api")
-                .setPredicateItems(new HashSet<ApiPredicateItem>() {{
-                    add(new ApiPathPredicateItem().setPattern("/ahas"));
-                    add(new ApiPathPredicateItem().setPattern("/product/**")
-                            .setMatchStrategy(SentinelGatewayConstants.PARAM_MATCH_STRATEGY_PREFIX));
-                }});
-        ApiDefinition api2 = new ApiDefinition("another_customized_api")
-                .setPredicateItems(new HashSet<ApiPredicateItem>() {{
-                    add(new ApiPathPredicateItem().setPattern("/**")
-                            .setMatchStrategy(SentinelGatewayConstants.PARAM_MATCH_STRATEGY_PREFIX));
-                }});
-        definitions.add(api1);
-        definitions.add(api2);
-        GatewayApiDefinitionManager.loadApiDefinitions(definitions);
     }
 
     private void initGatewayRules() {
         Set<GatewayFlowRule> rules = new HashSet<>();
-        rules.add(new GatewayFlowRule("aliyun_route")
-                .setCount(10)
-                .setIntervalSec(1)
-        );
-        rules.add(new GatewayFlowRule("aliyun_route")
-                .setCount(2)
-                .setIntervalSec(2)
-                .setBurst(2)
-                .setParamItem(new GatewayParamFlowItem()
-                        .setParseStrategy(SentinelGatewayConstants.PARAM_PARSE_STRATEGY_CLIENT_IP)
-                )
-        );
-        rules.add(new GatewayFlowRule("httpbin_route")
-                .setCount(10)
-                .setIntervalSec(1)
-                .setControlBehavior(RuleConstant.CONTROL_BEHAVIOR_RATE_LIMITER)
-                .setMaxQueueingTimeoutMs(600)
-                .setParamItem(new GatewayParamFlowItem()
-                        .setParseStrategy(SentinelGatewayConstants.PARAM_PARSE_STRATEGY_HEADER)
-                        .setFieldName("X-Sentinel-Flag")
-                )
-        );
-        rules.add(new GatewayFlowRule("httpbin_route")
+        rules.add(new GatewayFlowRule("example")
                 .setCount(1)
                 .setIntervalSec(1)
-                .setParamItem(new GatewayParamFlowItem()
-                        .setParseStrategy(SentinelGatewayConstants.PARAM_PARSE_STRATEGY_URL_PARAM)
-                        .setFieldName("pa")
-                )
-        );
-
-        rules.add(new GatewayFlowRule("some_customized_api")
-                .setResourceMode(SentinelGatewayConstants.RESOURCE_MODE_CUSTOM_API_NAME)
-                .setCount(5)
-                .setIntervalSec(1)
-                .setParamItem(new GatewayParamFlowItem()
-                        .setParseStrategy(SentinelGatewayConstants.PARAM_PARSE_STRATEGY_URL_PARAM)
-                        .setFieldName("pn")
-                )
         );
         GatewayRuleManager.loadRules(rules);
     }
