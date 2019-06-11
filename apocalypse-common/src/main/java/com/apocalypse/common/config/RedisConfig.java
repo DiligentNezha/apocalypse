7e1a54b4ad537d338b35e888bf522edc29283241
jingkaihui <kaihuijing@gmail.com>
2019-06-11 14:59:39 +0800

    redis cache 配置序列化

--- apocalypse-common/src/main/java/com/apocalypse/common/config/RedisConfig.java
+++ apocalypse-common/src/main/java/com/apocalypse/common/config/RedisConfig.java
@@ -1,17 +1,50 @@
 package com.apocalypse.common.config;
 
 import com.alibaba.fastjson.support.spring.GenericFastJsonRedisSerializer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.cache.CacheProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.io.Serializable;
 
 @Configuration
 public class RedisConfig {
+
+    @Autowired
+    private CacheProperties cacheProperties;
+
+    @Bean
+    public RedisCacheConfiguration redisCacheConfiguration() {
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig();
+
+        CacheProperties.Redis redisProperties = cacheProperties.getRedis();
+        GenericToStringSerializer keySerializer = new GenericToStringSerializer(Object.class);
+        GenericFastJsonRedisSerializer valueSerializer = new GenericFastJsonRedisSerializer();
+
+        config = config
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(keySerializer))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(valueSerializer));
+        if (redisProperties.getTimeToLive() != null) {
+            config = config.entryTtl(redisProperties.getTimeToLive());
+        }
+        if (redisProperties.getKeyPrefix() != null) {
+            config = config.prefixKeysWith(redisProperties.getKeyPrefix());
+        }
+        if (!redisProperties.isCacheNullValues()) {
+            config = config.disableCachingNullValues();
+        }
+        if (!redisProperties.isUseKeyPrefix()) {
+            config = config.disableKeyPrefix();
+        }
+        return config;
+    }
 
     @Bean(name = "redisTemplate")
     public RedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory) {
--- apocalypse-example/apocalypse-example-app/src/main/java/com/apocalypse/example/controller/CacheExampleController.java
+++ apocalypse-example/apocalypse-example-app/src/main/java/com/apocalypse/example/controller/CacheExampleController.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
+@CacheConfig(cacheNames = "spring-cache:example")
 @RequestMapping("/cache")
 public class CacheExampleController {
 
@@ -24,7 +26,7 @@ public class CacheExampleController {
     @Autowired
     private CacheManager cacheManager;
 
-    @Cacheable(cacheNames = "exampleCache")
+    @Cacheable
     @GetMapping("/query/{id}")
     public Rest<ExampleDO> query(@PathVariable("id") Long id) {
         ExampleDO exampleDO = exampleService.selectByPrimaryKey(id);
