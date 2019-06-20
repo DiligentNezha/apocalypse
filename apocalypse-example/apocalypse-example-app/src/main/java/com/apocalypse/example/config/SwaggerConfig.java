898342251890a8405e1d465569f1fc32ab01fdd5
jingkaihui <kaihuijing@gmail.com>
2019-06-20 21:03:44 +0800

    swagger配置 封装

--- apocalypse-example/apocalypse-example-app/src/main/java/com/apocalypse/example/config/SwaggerConfig.java
+++ apocalypse-example/apocalypse-example-app/src/main/java/com/apocalypse/example/config/SwaggerConfig.java
@@ -11,31 +11,31 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@Configuration
-@EnableSwagger2
-@EnableSwaggerBootstrapUI
+//@Configuration
+//@EnableSwagger2
+//@EnableSwaggerBootstrapUI
 public class SwaggerConfig {
 
     /**
      * swagger-bootstrap-ui默认访问地址是：http://${host}:${port}/doc.html
      * @return
      */
-    @Bean
-    public Docket createRestApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.apocalypse.example.controller"))
-                .paths(PathSelectors.any())
-                .build();
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Example模块")
-                .description("Example相关接口")
-                .version("1.0.0")
-                .build();
-    }
+//    @Bean
+//    public Docket createRestApi() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .apiInfo(apiInfo())
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("com.apocalypse.example.controller"))
+//                .paths(PathSelectors.any())
+//                .build();
+//    }
+//
+//    private ApiInfo apiInfo() {
+//        return new ApiInfoBuilder()
+//                .title("Example模块")
+//                .description("Example相关接口")
+//                .version("1.0.0")
+//                .build();
+//    }
 
 }
