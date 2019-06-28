ca786406d2f0fea4148742ba7bd17e01ce45ec97
jingkaihui <kaihuijing@gmail.com>
2019-06-28 14:24:13 +0800

    版本升级

---
+++ example/example-app/src/test/java/jdk8DateTest.java
@@ -0,0 +1,33 @@
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+
+/**
+ * @author <a href="jingkaihui@adpanshi.com">jingkaihui</a>
+ * @Description
+ * @date 2019/6/25
+ */
+@Slf4j
+public class jdk8DateTest {
+
+    /**
+     * Instant 代替 Date
+     */
+    @Test
+    public void instantTest() {
+        Instant now = Instant.now();
+        log.info("now:{}", now);
+    }
+
+    /**
+     * LocalDateTime 代替 Calendar
+     */
+    @Test
+    public void localDateTimeTest() {
+        LocalDateTime now = LocalDateTime.now();
+        log.info("now:{}", now);
+    }
+
+}
