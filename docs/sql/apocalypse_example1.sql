7b3e9a3cc443458a5a20640145b098ee11a59139
jingkaihui <kaihuijing@gmail.com>
2019-07-30 18:26:28 +0800

    SQL备份

--- docs/sql/apocalypse_example1.sql
+++ docs/sql/apocalypse_example1.sql
@@ -5,13 +5,13 @@
  Source Server Type    : MySQL
  Source Server Version : 50724
  Source Host           : localhost:3306
- Source Schema         : apocalypse_example
+ Source Schema         : apocalypse_example1
 
  Target Server Type    : MySQL
  Target Server Version : 50724
  File Encoding         : 65001
 
- Date: 30/07/2019 18:25:18
+ Date: 30/07/2019 18:25:28
 */
 
 SET NAMES utf8mb4;
@@ -106,71 +106,6 @@ CREATE TABLE `sharding_database_table_3`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `sharding_database_table_4`;
 CREATE TABLE `sharding_database_table_4`  (
-  `id` bigint(20) NOT NULL COMMENT '编号',
-  `remark` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '备注',
-  `deleted` bit(1) NOT NULL DEFAULT b'0' COMMENT '是否删除：1：已删除；0：未删除',
-  `create_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
-  `update_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '最后更新时间',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '分表' ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Table structure for sharding_table_0
--- ----------------------------
-DROP TABLE IF EXISTS `sharding_table_0`;
-CREATE TABLE `sharding_table_0`  (
-  `id` bigint(20) NOT NULL COMMENT '编号',
-  `remark` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '备注',
-  `deleted` bit(1) NOT NULL DEFAULT b'0' COMMENT '是否删除：1：已删除；0：未删除',
-  `create_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
-  `update_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '最后更新时间',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '分表' ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Table structure for sharding_table_1
--- ----------------------------
-DROP TABLE IF EXISTS `sharding_table_1`;
-CREATE TABLE `sharding_table_1`  (
-  `id` bigint(20) NOT NULL COMMENT '编号',
-  `remark` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '备注',
-  `deleted` bit(1) NOT NULL DEFAULT b'0' COMMENT '是否删除：1：已删除；0：未删除',
-  `create_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
-  `update_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '最后更新时间',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '分表' ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Table structure for sharding_table_2
--- ----------------------------
-DROP TABLE IF EXISTS `sharding_table_2`;
-CREATE TABLE `sharding_table_2`  (
-  `id` bigint(20) NOT NULL COMMENT '编号',
-  `remark` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '备注',
-  `deleted` bit(1) NOT NULL DEFAULT b'0' COMMENT '是否删除：1：已删除；0：未删除',
-  `create_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
-  `update_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '最后更新时间',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '分表' ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Table structure for sharding_table_3
--- ----------------------------
-DROP TABLE IF EXISTS `sharding_table_3`;
-CREATE TABLE `sharding_table_3`  (
-  `id` bigint(20) NOT NULL COMMENT '编号',
-  `remark` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '备注',
-  `deleted` bit(1) NOT NULL DEFAULT b'0' COMMENT '是否删除：1：已删除；0：未删除',
-  `create_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
-  `update_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '最后更新时间',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '分表' ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Table structure for sharding_table_4
--- ----------------------------
-DROP TABLE IF EXISTS `sharding_table_4`;
-CREATE TABLE `sharding_table_4`  (
   `id` bigint(20) NOT NULL COMMENT '编号',
   `remark` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '备注',
   `deleted` bit(1) NOT NULL DEFAULT b'0' COMMENT '是否删除：1：已删除；0：未删除',
