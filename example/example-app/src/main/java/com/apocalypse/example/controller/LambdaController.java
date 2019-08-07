b91547299589b4d73d9f0e0b81a26a68dcd677a1
jingkaihui <kaihuijing@gmail.com>
2019-08-07 14:04:09 +0800

    Lambda 案例

--- example/example-app/src/main/java/com/apocalypse/example/controller/LambdaController.java
+++ example/example-app/src/main/java/com/apocalypse/example/controller/LambdaController.java
@@ -1,7 +1,11 @@
 package com.apocalypse.example.controller;
 
 import cn.hutool.core.util.NumberUtil;
+import cn.hutool.core.util.RandomUtil;
+import com.alibaba.fastjson.JSONObject;
 import com.apocalypse.common.dto.Rest;
+import com.github.javafaker.Address;
+import com.github.javafaker.Faker;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
@@ -10,8 +14,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
+import java.util.function.Function;
 import java.util.function.IntPredicate;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -28,12 +38,56 @@ import java.util.stream.IntStream;
 public class LambdaController {
 
     @GetMapping("/predicate")
-    @ApiOperation(value = "predicate", notes = "Predicate测试")
-    public Rest<List<Integer>> hello() {
+    @ApiOperation(value = "Predicate", notes = "Predicate测试", consumes = "application/json")
+    public Rest<List<Integer>> predicate() {
         List<Integer> collect = IntStream.rangeClosed(2, 1000)
-                .filter(((IntPredicate) NumberUtil::isPrimes).and(value -> String.valueOf(value).length() > 2))
+                //素数并且第一位和第三位值一样大
+                .filter(((IntPredicate) NumberUtil::isPrimes).and(value -> value / 100 == value % 10))
                 .boxed().collect(Collectors.toList());
         return Rest.ok(collect);
     }
 
+    @GetMapping("/function")
+    @ApiOperation(value = "Function", notes = "Function测试", consumes = "application/json")
+    public Rest<String> function() {
+        Function<String, Integer> toInteger = Integer::valueOf;
+        //把String转成数字，再把数字转回去
+        Function<String, String> backToString = toInteger.andThen(String::valueOf);
+        return Rest.ok(backToString.apply("123"));
+    }
+
+    @GetMapping("/supply")
+    @ApiOperation(value = "Supply", notes = "Supply测试", consumes = "application/json")
+    public Rest<JSONObject> supply() {
+        JSONObject result = new JSONObject();
+        Supplier<Address> addressSupplier = Faker.instance(Locale.getDefault())::address;
+        Address address = addressSupplier.get();
+        Class<? extends Address> addressClass = address.getClass();
+        Method[] declaredMethods = addressClass.getDeclaredMethods();
+        Arrays.stream(declaredMethods)
+                .filter(method -> method.getParameterCount() == 0)
+                .forEach(method -> {
+                    try {
+                        result.put(method.getName(), method.invoke(address));
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    } catch (InvocationTargetException e) {
+                        e.printStackTrace();
+                    }
+                });
+        return Rest.ok(result);
+    }
+
+    @GetMapping("/comparator")
+    @ApiOperation(value = "Comparator", notes = "Comparator测试", consumes = "application/json")
+    public Rest<List<Integer>> comparator() {
+        List<Integer> collect = IntStream.rangeClosed(1, 10)
+                .map(operand -> operand + RandomUtil.randomInt(10))
+                .sorted()
+                .skip(5)
+                .limit(3)
+                .boxed()
+                .collect(Collectors.toList());
+        return Rest.ok(collect);
+    }
 }
